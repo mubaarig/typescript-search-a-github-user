c039423e0665d82b663210e791df1b2686ba55dc
Mubarig C <muba@Mubarigs-Air.home>
2024-03-25 16:19:10 +0100

    Components->Form, Card & CardList

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+interface Props {
+name: string;
+}
+
+const Card: React.FC<Props> = (): JSX.Element => {
+return <div></div>;
+};
+
+export default Card;
---
+++ src/components/CardList.tsx
@@ -0,0 +1,9 @@
+interface Props {
+name: string;
+}
+
+const CardList: React.FC<Props> = (): JSX.Element => {
+return <div></div>;
+};
+
+export default CardList;
