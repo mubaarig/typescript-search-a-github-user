c22c5d75f1cfe3b2f6318538197243141adad109
Mubarig C <muba@Mubarigs-Air.home>
2024-03-26 05:21:34 +0100

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from "./components/Form";
+import CardList from "./components/CardList";
 
-export default function App() {
-  return (
-    <div className="container mx-auto justify-center items-center h-screen">
-        <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'>
-            Search a GitHub User
-        </h1>
-        <Form onSubmit={function (userData: any): void {
-        throw new Error("Function not implemented.");
-      } } />
-        {/* <CardList cards={cards} /> */}
-    </div>
-)
+function App() {
+    const [cards, setCards] = useState<any>([]); // Assuming results structure
+
+    const addNewCard = (card: any) => {
+        setCards( [...cards, card]);
+    }
+
+    return (
+        <div className="container mx-auto justify-center items-center h-screen">
+            <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'>
+                Search a GitHub User
+            </h1>
+            <Form onSubmit={addNewCard} />
+            <CardList cards={cards} />
+        </div>
+    )
+
+
 }
+
+export default App;
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,9 +1,18 @@
-interface Props {
-name: string;
+import React from "react";
+import Card, { CardProps } from "./Card";
+
+interface CardListProps {
+  cards: CardProps[]; // Assuming CardProps is defined in Card component
 }
 
-const CardList: React.FC<Props> = (): JSX.Element => {
-return <div></div>;
+const CardList: React.FC<CardListProps> = (props) => {
+  return (
+    <div id="container" className="mt-10">
+      {props.cards.map((card, index) => (
+        <Card key={index} {...card} />
+      ))}
+    </div>
+  );
 };
 
 export default CardList;
